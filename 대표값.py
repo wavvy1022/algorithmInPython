fd659f4c4a3fd751f8edd89e1c5fedd4110bbe5a
ryun <run2070@naver.com>
2022-04-20 15:47:14 +0900

    대표값 풀이 수정(enumerate 추가)

--- 대표값.py
+++ 대표값.py
@@ -8,44 +8,21 @@ a = list(map(int, input().split()))
 resultVal = 0
 resultIdx = 0
 
-#수학점수 평균값 출력
-sum = 0
-for i in range(len(a)):
-    sum += a[i]
-#평균점수 반올림처리
-avg = round(sum/len(a))
-
-diffValArr = []
-for i in range(len(a)):
-#평균점수 차이값 절대값으로 변환
-    diffValue = abs(a[i]-avg)
-    diffValArr.append(diffValue)
-
-#차이값 배열에서 가장 최소값 출력
-minValue = min(diffValArr)
-
-minIdxArr = []
-#최소값이 단건인지 여러건인지 분기 처리
-if diffValArr.count(minValue)>=1:
-    for i in range(len(diffValArr)):
-        if diffValArr[i] == minValue:
-            #여러 최소값index값 배열에 append
-            minIdxArr.append(i)
-    maxValArr = []
-    for i in range(len(minIdxArr)):
-        maxValArr.append(a[minIdxArr[i]])
-    maxValue = max(maxValArr)
-    maxValIdx = a.index(maxValue)
-    print(maxValue,maxValIdx+1, sep=' ')
-else:
-    #단건일 경우 최소값 출력
-    minIdx = diffValArr.index(minValue)
-    minVal = a[minIdx]
-    minValIdx = a.index(minIdx)
-    print(minVal,minValIdx+1, sep=' ')
-
-
-
-
-
-
+#수학점수 평균값 출력및 반올림처리
+avg = round(sum(a)/len(a))
+# float("inf") 양의 무한대(최대값으로 설정)
+min = float("inf")
+for idx, x in enumerate(a):
+#enumerate : 배열 각각요소의 값과 index값을 쌍으로 대응해 주는 함수
+    tmp=abs(x-avg)
+    if(tmp<min):
+        min = tmp
+        score = x
+        res = idx+1
+    #elif => else if
+    elif tmp==min:
+        if x>score:
+            #평균차가 동일한 경우 점수가 높은 케이스의 경우에만 답으로 설정
+            score=x
+            res=idx+1
+print(avg,res, sep=' ')
