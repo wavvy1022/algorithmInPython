26b203582734e600ec24e6c89384731bf8f7acce
ryun <run2070@naver.com>
2022-04-21 16:29:34 +0900

    자리수의 합 다른 풀이

--- 자리수의 합.py
+++ 자리수의 합.py
@@ -13,11 +13,21 @@ result = 0
 
 #자리수 합산 function
 def digit_sum(num):
-    #각 자리수값 배열로 변환
-    arr = [int(x) for x in str(num)]
-    #배열의 합산처리
-    sumNum = sum(arr)
-    return sumNum
+    # #각 자리수값 배열로 변환
+    # arr = [int(x) for x in str(num)]
+    # #배열의 합산처리
+    # sumNum = sum(arr)
+    # return sumNum
+
+    # 다른 풀이
+    sum = 0
+    while num>0:
+        # 10으로 나누었을때의 나머지값을 sum변수에 더함
+        sum+=num%10
+        # 10으로 나눴을때의 몫값을 num변수에 할당
+        # 나머지가 1자리수까지 반복 => 각각 자리수에 대한 합산처리
+        num=num//10
+    return sum
 
 for x in a:
     #자리수 합산 최대값
