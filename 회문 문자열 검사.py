21b1664613a762544bf451ffa5a6307f556bd4b0
ryun <run2070@naver.com>
2022-04-23 23:27:49 +0900

    회문 검사 다른 풀이

--- 회문 문자열 검사.py
+++ 회문 문자열 검사.py
@@ -18,16 +18,27 @@ for T in range(n):
 
     # 내장함수 안쓰는 ver
 
-    reverseArr = []
-    # for문 역순으로 수행해서 reverse
-    for i in range(len(strArr)-1,-1,-1):
-        reverseArr.append(strArr[i])
+    # reverseArr = []
+    # # for문 역순으로 수행해서 reverse
+    # for i in range(len(strArr)-1,-1,-1):
+    #     reverseArr.append(strArr[i])
     
-    reverseTxt = ''.join(reverseArr)
+    # reverseTxt = ''.join(reverseArr)
+
+    #다른 풀이3
+    size = len(text)
+    # 문자열을 반으로 나눠서 나눈값을 기준으로 대칭된 텍스트가 같은지 체크 후
+    # 하나라도 다른 텍스트가 나올 경우 for문 break
+    for i in range(size//2):
+        if text[i]!=text[-1-i]:
+            print("#%d NO" %(T+1))
+            break
+    else:
+        print("#%d YES" %(T+1))
 
     # 케이스별 회문문자 여부 출력
 
-    if text==reverseTxt:
-        print("#%d %s" %(T+1,"YES"))
-    else:
-        print("#%d %s" %(T+1,"NO"))
+    # if text==reverseTxt:
+    #     print("#%d %s" %(T+1,"YES"))
+    # else:
+    #     print("#%d %s" %(T+1,"NO"))
