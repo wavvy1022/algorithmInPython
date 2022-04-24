e50dd5fe76a2d89caef188220b5a7cb14f0e9633
ryun <run2070@naver.com>
2022-04-24 23:44:18 +0900

    숫자만 추출 작업중 숫자추출 완료 약수 개수 카운트 작업중

---
+++ 숫자만 추출.py
@@ -0,0 +1,21 @@
+import sys
+import re
+sys.stdin = open("input.txt","rt")
+
+t= input()
+
+# 숫자 정규표현식
+regex = re.compile('\d')
+regex.search
+
+numArr = []
+
+for i in t:
+    # 정규표현식으로 입력된 텍스트에서 숫자인 요소들만 추출
+    if regex.search(i) is not None:
+        numArr.append(i)
+
+# 숫자인 요소들만 추출된 배열을 문자열로 변환후 다시 int로 변환시킴으로서
+# 앞자리의 0은 자연수화 하면서 삭제처리 되도록함
+num = int("".join(numArr))
+print(num)
