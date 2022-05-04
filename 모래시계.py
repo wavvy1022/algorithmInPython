dde3501566d53d5c979d7d06743c57011e020249
ryun <run2070@naver.com>
2022-05-04 15:57:56 +0900

    총합 수정 완료

--- 모래시계.py
+++ 모래시계.py
@@ -11,6 +11,10 @@ a2 = [list(map(int, input().split())) for _ in range(m)]
 # 모래시계 영역 총합 변수
 res = 0
 
+# 탐색 영역 포인터
+s = 0
+e = n
+
 for i in range(0,len(a)):
     # 배열 회전 수정 시작
     for j in a2:
@@ -32,21 +36,16 @@ for i in range(0,len(a)):
     # 배열 회전 수정 완료
 
     # 모래시계 모양 영역 값 추출 시작
-    # 탐색 영역 포인터
-    s = n-1
-    e = 0
-    # for문 돌면서 res에 탐색영역 총합계산 후 포인터 이동
-    for k in range(e, s):
-        #2차원 배열 중간지점 분기처리후 포인터 처리
-        if (n//2)+1>i+1:
-            print(0)
-            res += a[i][k]
-            e+=1
-            s-=1
-        else:
-            print(1)
-            res += a[i][k]
-            e-=1
-            s+=1
-
+    # for문 돌면서 res에 탐색영역 총합계산 후 포인터 처리
+    for k in range(s, e):
+        res += a[i][k]
+    #2차원 배열 중간지점 분기처리
+    if (n//2)>i:
+        # 2차원배열 중간지점 이전에는 e,s 포인터 사이 간격 감소 처리
+        s+=1
+        e-=1
+    else:
+        # 중간지점 이후 e,s포인터 사이 간경 증가 처리
+        s-=1
+        e+=1
 print(res)
