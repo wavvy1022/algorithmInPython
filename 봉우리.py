08be22c3ac50e3f6a9e7d6939f0f0c06f4bf321c
ryun <run2070@naver.com>
2022-05-06 16:54:04 +0900

    오류 수정(최대값 동일한 case제외)

--- 봉우리.py
+++ 봉우리.py
@@ -17,7 +17,7 @@ for i in range(0,len(a)):
 cnt = 0
 
 # 0으로 초기화 된 영역을 제외한 2차원 배열 2중 for문
-for i in range(1,len(a)-1):
+for i in range(1,n+1):
     for j in range(1,n+1):
         # 요소의 상,하,좌,우 영역 배열에 append후 해당 요소가 배열에서 최대값일 경우
         # cnt +1처리
@@ -26,7 +26,7 @@ for i in range(1,len(a)-1):
         arr.append(a[i+1][j])
         arr.append(a[i][j-1])
         arr.append(a[i][j+1])
-        arr.append(a[i][j])
-        if max(arr)==a[i][j]:
+        # arr최대값과 봉우리 판별요소 비교해서 arr최대값보다 클 경우 카운트 추가(같은case 제외!)
+        if max(arr)<a[i][j]:
             cnt += 1
 print(cnt)
