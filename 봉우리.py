ffcc8feaf5ec63af58700d2817da8fd4f7a91072
ryun <run2070@naver.com>
2022-05-06 15:59:38 +0900

    주석 추가

--- 봉우리.py
+++ 봉우리.py
@@ -3,17 +3,24 @@ sys.stdin = open("input.txt","rt")
 
 n = int(input())
 a = [list(map(int, input().split())) for _ in range(n)]
+
+# 2차원 배열 a의 상,하 0으로 초기화
 a.insert(0, [0]*n)
 a.append([0]*n)
 
+# 2차원 배열 a의 좌,우 0으로 초기화
 for i in range(0,len(a)):
     a[i].insert(0,0)
     a[i].append(0)
 
+# 봉우리 개수 변수
 cnt = 0
+
+# 0으로 초기화 된 영역을 제외한 2차원 배열 2중 for문
 for i in range(1,len(a)-1):
-    
     for j in range(1,n+1):
+        # 요소의 상,하,좌,우 영역 배열에 append후 해당 요소가 배열에서 최대값일 경우
+        # cnt +1처리
         arr = []
         arr.append(a[i-1][j])
         arr.append(a[i+1][j])
